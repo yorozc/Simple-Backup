eb0ca4ebc6db088ae6065550894d7512d5a40df5
yorozc <yorozcolop49@gmail.com>
2025-08-27 22:00:59 -0700

    files copy over to drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, shutil, sys, getpass, pyinputplus as pypi
 
+# REMEMBER TO PUT IN README TO FORMAT TO NTFS DOES NO WORK IN exFAT
 
 def findExternalDrive(): #Used to find external drives for backup
     drives = os.listdir(f"/media/{getpass.getuser()}")
@@ -14,20 +15,11 @@ def findExternalDrive(): #Used to find external drives for backup
         return driveChoice.upper()
 
 user_drive = findExternalDrive()
-
-# if len(sys.argv) < 3:
-#     print("Usage: python main.py [source_directory] [backup_directory]")
-#     sys.exit(1)
-
-#ask for which directory they want to back up
-
-source_dir = input("Insert source directory path:(absolute path) ")
-# source_dir = sys.argv[1]
-
-#get directory that you want to back up to
-
-backup_directory = input("Input backup directory path:(absolute path) ")
-# backup_directory = sys.argv[2]
+backup_drive = f"/media/{getpass.getuser()}/{user_drive}"
+# prompt user for dir they want backed up and copy contents to drive
+# add some input validation later
+source_dir = pypi.inputFilepath(prompt="Please enter the absolute path of the directory: ")
+print(source_dir)
 
 for item in os.listdir(source_dir):
 
@@ -36,15 +28,16 @@ for item in os.listdir(source_dir):
 
         if os.path.isdir(full_path): #checks if dir
             print(f'{item} is a directory')
-            shutil.move(full_path, backup_directory + f'/{item}')
+            shutil.copytree(full_path, backup_drive + f'/{item}')
 
         elif os.path.isfile(full_path): #checks if file
             print(f'{item} is a file.')
-            shutil.move(full_path, backup_directory + f'/{item}')
+            shutil.copy(full_path, backup_drive + f'/{item}')
             
         else: 
             print(f'{item} is neither. {item} not moved.')
     else:
         print("Path does not exist.")
 
-print("Move complete!")
+#after prompting user to choose drive, prompt user to select 
+#directories that you want to back up
