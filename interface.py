86dac9a1f99c44e477cd93982919668f3ec71443
yorozc <yorozcolop49@gmail.com>
2025-08-29 23:22:51 -0700

    added basic print statemenet UI for interface.py. Making decision for it config file exists or not

--- interface.py
+++ interface.py
@@ -1,2 +1,23 @@
 # cmd line interface
 import main
+import os
+
+print("===========================================")
+print("|                                         |")
+print("|                                         |")
+print("|                                         |")
+print("|        Welcome to Simple Backup         |")
+print("|                                         |")
+print("|                                         |")
+print("|                                         |")
+print("===========================================")
+
+# check if user has config file in same dir to check if it is their first time
+CONFIG_BACKUP = "backup_config.json"
+if os.path.isfile(CONFIG_BACKUP):
+    pass
+
+else: # file doesn't exist, make user select drive and directories to backup
+    print("Config is required during first run.")
+    print("====================================")
+    backup_drive = main.findExternalDrive() #chooses drive to backup on
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, shutil, sys, getpass, pyinputplus as pypi
 
 # REMEMBER TO PUT IN README TO FORMAT TO NTFS DOES NO WORK IN exFAT
-# save user info through JSON format or txt file? (find best soltuion to save user information)
+# save user info through JSON format or txt file? (find best solution to save user information)
 
 def findExternalDrive(): #Used to find external drives for backup
     drives = os.listdir(f"/media/{getpass.getuser()}")
@@ -13,36 +13,38 @@ def findExternalDrive(): #Used to find external drives for backup
         return 
     else:
         driveChoice = pypi.inputChoice(drives, prompt="Please select one of the drives: ",blank=True)
-        print(f"Drive {driveChoice} is chosen.")
+        print(f"Drive {driveChoice} is chosen. This is now your backup drive.")
         return driveChoice.upper()
 
-user_drive = findExternalDrive()
+# user_drive = findExternalDrive()
 
-backup_drive = f"/media/{getpass.getuser()}/{user_drive}"
+# backup_drive = f"/media/{getpass.getuser()}/{user_drive}"
 
 # prompt user for dir they want backed up and copy contents to drive
 
 # add some input validation later
-source_dir = pypi.inputFilepath(prompt="Please enter the absolute path of the directory you want to backup: ")
-print(source_dir)
+def copyDirs(backup_drive: str):
+    source_dir = pypi.inputFilepath(prompt="Please enter the absolute path of the directory you want to backup:" \
+                                    "\n If multiple directories, separate with a space. ")
+    print(source_dir)
 
-for item in os.listdir(source_dir):
+    for item in os.listdir(source_dir):
 
-    full_path = os.path.join(source_dir, item) #construct full path for checks
-    if os.path.exists(full_path):
+        full_path = os.path.join(source_dir, item) #construct full path for checks
+        if os.path.exists(full_path):
 
-        if os.path.isdir(full_path): #checks if dir
-            print(f'{item} is a directory. Backup complete.')
-            shutil.copytree(full_path, backup_drive + f'/{item}')
+            if os.path.isdir(full_path): #checks if dir
+                print(f'{item} is a directory. Backup complete.')
+                shutil.copytree(full_path, backup_drive + f'/{item}')
 
-        elif os.path.isfile(full_path): #checks if file
-            print(f'{item} is a file. Backup complete.')
-            shutil.copy(full_path, backup_drive + f'/{item}')
-            
-        else: 
-            print(f'{item} is neither. {item} not moved.')
-    else:
-        print("Path does not exist.")
+            elif os.path.isfile(full_path): #checks if file
+                print(f'{item} is a file. Backup complete.')
+                shutil.copy(full_path, backup_drive + f'/{item}')
+                
+            else: 
+                print(f'{item} is neither. {item} not moved.')
+        else:
+            print("Path does not exist.")
 
 #after prompting user to choose drive, prompt user to select 
 #directories that you want to back up
